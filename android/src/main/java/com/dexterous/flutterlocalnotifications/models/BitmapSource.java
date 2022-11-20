fb1a4907ae19e9dace7d48e7768cbabf7d728d1c
hnguyen48206 <hoangbich1989@gmail.com>
2022-11-20 16:54:56 +0700

    abc

--- android/src/main/java/com/dexterous/flutterlocalnotifications/models/BitmapSource.java
+++ android/src/main/java/com/dexterous/flutterlocalnotifications/models/BitmapSource.java
@@ -3,10 +3,8 @@ package com.dexterous.flutterlocalnotifications.models;
 import androidx.annotation.Keep;
 
 @Keep
-public enum IconSource {
+public enum BitmapSource {
   DrawableResource,
-  BitmapFilePath,
-  ContentUri,
-  FlutterBitmapAsset,
+  FilePath,
   ByteArray
 }
--- android/src/main/java/com/dexterous/flutterlocalnotifications/models/SoundSource.java
+++ android/src/main/java/com/dexterous/flutterlocalnotifications/models/SoundSource.java
@@ -3,7 +3,7 @@ package com.dexterous.flutterlocalnotifications.models;
 import androidx.annotation.Keep;
 
 @Keep
-public enum ScheduledNotificationRepeatFrequency {
-  Daily,
-  Weekly
+public enum SoundSource {
+  RawResource,
+  Uri
 }
